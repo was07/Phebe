341d8683ab4f37a0f25d1392a2ba6e71252d9abf
David Reilly <greyblue9@gmail.com>
2022-02-13 12:03:45 +0000

    Add other commands to Python.py

--- commands/Python.py
+++ commands/Python.py
@@ -1,16 +1,24 @@
-import disnake
+from base import *
 from contextlib import redirect_stderr, redirect_stdout
 from disnake import Color
-from disnake.ext import commands
 from io import StringIO
 from re import subn, DOTALL
 from tempfile import NamedTemporaryFile
 from traceback import print_exc 
 from types import CodeType
-from typing import Any
+import getdoc
+from init import Formatted
+import re
+import requests
 
 
-class Eval(commands.Cog):
+class Python(commands.Cog):
+    def __init__(self, bot: Bot) -> None:
+        super().__init__()
+        self.bot = bot
+        type(self).__cog_name__ = "Python"
+
+
     @commands.command()
     async def e(self, ctx, *, source: str=""):
         """Run and get output of a python code"""
@@ -61,5 +69,81 @@ class Eval(commands.Cog):
             color=color,
         ))
 
-def setup(bot: commands.Bot):
-    bot.add_cog(Eval())
+
+    @commands.command()
+    async def d(self, ctx, symbol: str='', nparas: int=1):
+        """Get the Documentation of a python object"""
+        symbol = symbol.strip('`')
+        if symbol == 'Creds':
+            await ctx.send(embed=disnake.Embed(title='Credits',description="Made by `Greyblue92`, `Was'` and `Pancake`, `sz_skill`"))
+        elif symbol:
+            doc: Formatted
+            item, doc, url = getdoc.getdoc(symbol, nparas)
+            await ctx.send(embed=disnake.Embed(
+                title = symbol,
+                url=url,
+                description = str(doc),
+                color=disnake.Color.blue()
+            ))
+
+    
+    @commands.command(alias="pypi")
+    async def pypi(self, ctx, name=None):
+        if name is None:
+            await ctx.send('Package name required.')
+            return
+
+        resp = requests.get(f'https://pypi.org/pypi/{name}/json')
+        if resp.status_code == 404:
+            await ctx.send(embed=disnake.Embed(title=f"Could not find {name}"))
+            return
+
+        data = resp.json()
+        info = data['info']
+        url = info['project_url']
+
+        rawdescr = info['description']
+        text = format(rawdescr)
+        await ctx.send(embed=disnake.Embed(
+            title=name, 
+            url=url,
+            description=text,
+            color=disnake.Color.blue(),
+        ))
+
+
+
+def format(text, lines=6):
+    res = ""
+    text = re.compile(
+      r'\s*</?(?:(?!\n[\n\r#]).)*>\s*',
+      re.DOTALL
+    ).subn('\n\n', text)[0]
+    text = text.replace('```python', '__start_py')
+    l = 0
+    for line in text.splitlines():
+        line = line.strip()
+        l += 1
+        if line.startswith('##'):
+            res += f"*{line.lstrip('##').strip()}*"
+        elif line.startswith('#'):
+            res += f"**{line.lstrip('#').strip()}**"
+        elif line.startswith(':'):
+            res += f"**{line.lstrip(':').strip()}**"
+        elif line.startswith('*'):
+            res += f'__{line.strip("*").strip()}__'
+        elif line.startswith('|'):
+            res += f"**{line.lstrip('|').strip()}**"
+        elif line.startswith('||'):
+            res += f"**{line.lstrip('||').strip()}**"
+        elif line.startswith('. ...'):
+            res += f"**{line.lstrip('. ...').strip()}**"
+        else:
+            res += line
+        res += '\n'
+        if l >= lines and '__start_py' in res and '```' not in res:
+            res += '```'
+            break 
+
+    res = res.replace('__start_py', '```py')
+    return res.rstrip() + ' ...'
