2fb3578c92d3c4a37a391769a34d1b44f673448a
David Reilly <greyblue9@gmail.com>
2022-03-27 08:51:07 +0000

    Add Fact cog and factgen.py

---
+++ commands/Fact.py
@@ -0,0 +1,26 @@
+#this one is in construction
+from base import *
+from factgen import random_fact
+from disnake import Color
+
+class Fact(commands.Cog):
+    def __init__(self, bot: Bot):
+        super().__init__()
+        self.bot = bot
+
+    @commands.command()
+    async def fact(self, ctx):
+        text, source = random_fact()
+        
+        embed = Embed(
+          title="Random Fact",
+          description=text,
+          url=source,
+          color=Color.yellow(),
+        )
+        embed.set_footer(text=f"Source: {source}")
+        await ctx.reply(
+          "Here's your random fact!",
+          embed=embed
+        )
+
